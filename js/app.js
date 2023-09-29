bb5dfea50f6d3f3e116f5ac04bf3adbb7cc568bf
abrahan <abrahandev@gmail.com>
2023-09-29 23:16:22 +0200

    Validacion terminada con mensaje de alerta

--- js/app.js
+++ js/app.js
@@ -18,5 +18,21 @@ function validarFormulario(e) {
 }
 
 function mostrarAlerta(mensaje) {
+    const alertaPrevia = document.querySelector('.alerta')
 
+    if (!alertaPrevia) {
+        const alerta = document.createElement('p')
+        alerta.classList.add('bg-red-100', 'alerta', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-lg', 'mx-auto', 'mt-6', 'text-center');
+
+        alerta.innerHTML = `
+        <strong class="font-bold">Error!</strong>
+        <span class="block sm:inline" >${mensaje}</span>
+    `;
+
+        formulario.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove()
+        }, 3000);
+    }
 }
