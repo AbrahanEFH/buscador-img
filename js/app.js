aaae20771ce01054f70bf15ef878aca9f6a0c7f7
abrahan <abrahandev@gmail.com>
2023-09-30 13:46:45 +0200

    paginas con sus botones agregadas

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const resultado = document.querySelector('#resultado');
 const formulario = document.querySelector('#formulario');
+const paginacionDiv = document.querySelector('#paginacion');
 
 const resgistroPorPagina = 40;
 let totalPaginas;
@@ -45,7 +46,7 @@ function mostrarAlerta(mensaje) {
 
 function buscarImagenes(termino) {
     const key = '39747926-dd78a22f07dfb931847a676ec'
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=50`
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${resgistroPorPagina}`
 
     // Consultando la API
     fetch(url)
@@ -104,4 +105,17 @@ function mostrarImagenes(imagenes) {
 function imprimirPaginador() {
     iterador = crearPaginador(totalPaginas)
 
+    while (true) {
+        const { value, done } = iterador.next();
+        if (done) return;
+
+        //Caso contrario, genera un boton por cada elemento en el generador
+        const boton = document.createElement('a');
+        boton.href = '#';
+        boton.dataset.pagina = value;
+        boton.textContent = value;
+        boton.classList.add('siguiente', 'bg-yellow-400', 'px-4', 'py-1', 'mr-2', 'font-bold', 'mb-10', 'uppercase', 'rounded');
+
+        paginacionDiv.appendChild(boton);
+    }
 }
